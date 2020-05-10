08ffa08cc9ffe52534f0876cc4d6b030252ddc21
egeergul <ege.ergul@ug.bilkent.edu.tr>
2020-05-10 15:23:32 +0300

    Made only one week time slot selectable for reservation making process!

--- app/src/main/java/android/example/fireapp/MakeReservationCustomerP1.java
+++ app/src/main/java/android/example/fireapp/MakeReservationCustomerP1.java
@@ -7,6 +7,12 @@ import android.os.Bundle;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MakeReservationCustomerP1 extends AppCompatActivity {
     CalendarView calendar;
     TextView tvDate;
@@ -20,10 +26,19 @@ public class MakeReservationCustomerP1 extends AppCompatActivity {
         calendar = (CalendarView)findViewById(R.id.calendarView);
         tvDate = (TextView)findViewById(R.id.txtDate);
 
+        //Make previous dates unclickable
+        calendar.setMinDate((new Date().getTime()));
+
+        //Make dates after one week unclickable
+        Date today = new Date();
+        Date weekAfter = new Date(today.getTime() + 7*(1000 * 60 * 60 * 24));
+        calendar.setMaxDate( weekAfter.getTime());
+
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = dayOfMonth + "/" + month + "/" + year;
+                date += "\n Please select a time now!";
                 tvDate.setText(date);
             }
         });
