cb91908b77dadbc864775a6232d004d8b15217f5
alieren_gunaltili <alierengunaltili@gmail.com>
2020-05-17 01:55:35 +0300

    adding two pages of the customer help

---
+++ app/src/main/java/android/example/fireapp/HowToMakeReservation.java
@@ -0,0 +1,14 @@
+package android.example.fireapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HowToMakeReservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.howtores);
+    }
+}
