7384558459322c0d6f09561739e4a0e440bed891
alieren_gunaltili <alierengunaltili@gmail.com>
2020-05-18 22:07:19 +0300

    Upload image to restaurant

---
+++ app/src/main/java/android/example/fireapp/Upload.java
@@ -0,0 +1,33 @@
+package android.example.fireapp;
+
+public class Upload { //our uploaded image have 2 main properties : specific url and the name user typed in.
+    private String mName;
+    private String mImageURL;
+
+    public Upload(){ //default constructor
+    }
+
+    public Upload(String mName, String mImageURL){ //constructor
+        if(mName.trim().equals("")){
+            mName = "no name";
+        }
+        this.mName = mName;
+        this.mImageURL = mImageURL;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmImageURL() {
+        return mImageURL;
+    }
+
+    public void setmImageURL(String mImageURL) {
+        this.mImageURL = mImageURL;
+    }
+}
