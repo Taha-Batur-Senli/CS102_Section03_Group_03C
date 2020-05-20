4d9613ee65f895926e710b2f10b5cb13dd095c4e
alieren_gunaltili <alierengunaltili@gmail.com>
2020-05-20 03:00:49 +0300

    changed background upload_image_full screen of upload pic

--- app/src/main/java/android/example/fireapp/ChooseUploadType.java
+++ app/src/main/java/android/example/fireapp/ChooseUploadType.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,6 +20,8 @@ public class ChooseUploadType extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_choose_upload_type);
         view = findViewById(R.id.imageView60);
         line = findViewById(R.id.imageView61);
--- app/src/main/java/android/example/fireapp/SeatingPlanPicsRecycler.java
+++ app/src/main/java/android/example/fireapp/SeatingPlanPicsRecycler.java
@@ -56,6 +56,5 @@ public class SeatingPlanPicsRecycler extends AppCompatActivity {
                         .show();
             }
         });
-
     }
 }
